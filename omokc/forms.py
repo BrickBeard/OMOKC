2481b5653e9248d5bd5d2489f51461659bc0cfc5
Smith Family <TheSmiths@Smiths-iMac.local>
2017-06-10 20:59:56 -0500

    delete all

--- omokc/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
-
-class ContactForm(FlaskForm):
-    name = TextField("Name", [validators.DataRequired('Please enter your name.')])
-    email = TextField("Email", [validators.DataRequired('Please enter your email.'), validators.Email('Please enter your email.')])
-    # subject = StringField("Subject")
-    message = TextAreaField("Message", [validators.DataRequired('Please enter a message.')])
-    submit = SubmitField("Send")
